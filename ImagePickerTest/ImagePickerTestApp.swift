9223879389fd4d151503d7c6e8448749d1be0db0
Y. Lai <yachieh@gmail.com>
2022-12-26 17:23:44 -0800

    Initial Commit

---
+++ ImagePickerTest/ImagePickerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePickerTestApp.swift
+//  ImagePickerTest
+//
+//  Created by YACHIEH LAI on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ImagePickerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImagePickerTestUITests/ImagePickerTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ImagePickerTestUITestsLaunchTests.swift
+//  ImagePickerTestUITests
+//
+//  Created by YACHIEH LAI on 12/26/22.
+//
+
+import XCTest
+
+final class ImagePickerTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
